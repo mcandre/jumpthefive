061a67a285bd5ed65493ac9182c83ddb198bce70
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 16:36:15 -0600

    update editorconfig

--- Shakefile.hs
+++ Shakefile.hs
@@ -4,48 +4,48 @@ import System.Directory as Dir
 
 main :: IO ()
 main = do
-  let tarball = "dist/jumpthefive-0.0.1.tar.gz"
-  homeDir <- Dir.getHomeDirectory
+    let tarball = "dist/jumpthefive-0.0.1.tar.gz"
+    homeDir <- Dir.getHomeDirectory
 
-  shakeArgs shakeOptions{ shakeFiles="dist" } $ do
-    want ["dist/bin/jumpthefive" <.> exe]
+    shakeArgs shakeOptions{ shakeFiles="dist" } $ do
+        want ["dist/bin/jumpthefive" <.> exe]
 
-    "dist/bin/jumpthefive" <.> exe %> \out ->
-      cmd_ "cabal" "install" "--bindir" "dist/bin"
+        "dist/bin/jumpthefive" <.> exe %> \out ->
+            cmd_ "cabal" "install" "--bindir" "dist/bin"
 
-    phony "hlint" $
-      cmd_ "hlint" "."
+        phony "hlint" $
+            cmd_ "hlint" "."
 
-    phony "lint" $
-      need ["hlint"]
+        phony "lint" $
+            need ["hlint"]
 
-    phony "test" $ do
-      need ["dist/bin/jumpthefive" <.> exe]
-      cmd_ $ "dist/bin/jumpthefive" <.> exe
+        phony "test" $ do
+            need ["dist/bin/jumpthefive" <.> exe]
+            cmd_ $ "dist/bin/jumpthefive" <.> exe
 
-    phony "install" $
-      cmd_ "cabal" "install"
+        phony "install" $
+            cmd_ "cabal" "install"
 
-    phony "uninstall" $ do
-      cmd_ "ghc-pkg" "unregister" "--force" "jumpthefive"
-      removeFilesAfter homeDir ["/.cabal/bin/jumpthefive" <.> exe]
+        phony "uninstall" $ do
+            cmd_ "ghc-pkg" "unregister" "--force" "jumpthefive"
+            removeFilesAfter homeDir ["/.cabal/bin/jumpthefive" <.> exe]
 
-    phony "build" $
-      cmd_ "cabal" "build"
+        phony "build" $
+            cmd_ "cabal" "build"
 
-    phony "haddock" $
-      cmd_ "cabal" "haddock"
+        phony "haddock" $
+            cmd_ "cabal" "haddock"
 
-    tarball %> \_ -> do
-      need ["build", "haddock"]
-      cmd_ "cabal" "sdist"
+        tarball %> \_ -> do
+            need ["build", "haddock"]
+            cmd_ "cabal" "sdist"
 
-    phony "sdist" $
-      need [tarball]
+        phony "sdist" $
+            need [tarball]
 
-    phony "publish" $ do
-      need ["sdist"]
-      cmd_ "cabal" "upload" tarball
+        phony "publish" $ do
+            need ["sdist"]
+            cmd_ "cabal" "upload" tarball
 
-    phony "clean" $
-      cmd_ "cabal" "clean"
+        phony "clean" $
+            cmd_ "cabal" "clean"
--- src/JumpTheFive.hs
+++ src/JumpTheFive.hs
@@ -1,10 +1,10 @@
 -- |
 -- JumpTheFive offers symmetric encryption for phone numbers.
 module JumpTheFive (
-    encrypt,
-    encrypt',
-    preprocess
-  ) where
+        encrypt,
+        encrypt',
+        preprocess
+    ) where
 
 import Data.Char (toUpper)
 import Control.Parallel.Strategies (parMap, rseq)
@@ -16,35 +16,35 @@ encrypt = parMap rseq (encrypt' . preprocess)
 -- | preprocess handles alphabetical phone digit aliases.
 preprocess :: Char -> Char
 preprocess c
-  | c `elem` ['a' .. 'z'] = (preprocess . toUpper) c
-  | otherwise = case c of
-    'A' -> '2'
-    'B' -> '2'
-    'C' -> '2'
-    'D' -> '3'
-    'E' -> '3'
-    'F' -> '3'
-    'G' -> '4'
-    'H' -> '4'
-    'I' -> '4'
-    'J' -> '5'
-    'K' -> '5'
-    'L' -> '5'
-    'M' -> '6'
-    'N' -> '6'
-    'O' -> '6'
-    'P' -> '7'
-    'Q' -> '7'
-    'R' -> '7'
-    'S' -> '7'
-    'T' -> '8'
-    'U' -> '8'
-    'V' -> '8'
-    'W' -> '9'
-    'X' -> '9'
-    'Y' -> '9'
-    'Z' -> '9'
-    c' -> c'
+    | c `elem` ['a' .. 'z'] = (preprocess . toUpper) c
+    | otherwise = case c of
+        'A' -> '2'
+        'B' -> '2'
+        'C' -> '2'
+        'D' -> '3'
+        'E' -> '3'
+        'F' -> '3'
+        'G' -> '4'
+        'H' -> '4'
+        'I' -> '4'
+        'J' -> '5'
+        'K' -> '5'
+        'L' -> '5'
+        'M' -> '6'
+        'N' -> '6'
+        'O' -> '6'
+        'P' -> '7'
+        'Q' -> '7'
+        'R' -> '7'
+        'S' -> '7'
+        'T' -> '8'
+        'U' -> '8'
+        'V' -> '8'
+        'W' -> '9'
+        'X' -> '9'
+        'Y' -> '9'
+        'Z' -> '9'
+        c' -> c'
 
 -- | encrypt' symmetrically obfuscates an individual phone digit.
 encrypt' :: Char -> Char
